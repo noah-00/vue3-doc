07537cbeaeb1cd8066d518ac574aa3b8e7ec756d
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-09-18 12:22:26 +0900

    ✨ computed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 // import TemplateLiteral from "./components/TemplateLiteral.vue";
-import ListRendering from "./components/ListRendering.vue";
+// import ListRendering from "./components/ListRendering.vue";
+import Computed from "./components/Computed.vue";
 </script>
 
 <template>
   <div>
     <!-- <TemplateLiteral /> -->
-    <ListRendering />
+    <!-- <ListRendering /> -->
+    <Computed />
   </div>
 </template>
---
+++ src/components/Computed.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const inputText = ref("");
+const computedText = computed(() => {
+  return inputText.value.length;
+});
+
+// Date.now() はリアクティブな依存ではないため、次の算出プロパティは二度と更新されないことを意味します:
+const now = computed(() => Date.now());
+</script>
+
+<template>
+  <div>
+    <h1>Computed</h1>
+    <input v-model="inputText" />
+    <p>Input text: {{ computedText }}</p>
+  </div>
+</template>
