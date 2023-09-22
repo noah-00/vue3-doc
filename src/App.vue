179c096c4d6d019d9b7f97d6cb9b1511ec5d8682
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-09-22 21:48:48 +0900

    ✨ slot

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,8 @@
 // import Watch from "./components/Essentials/Watch.vue";
 // import Ref from "./components/Essentials/Ref.vue";
 // import ComponentParent from "./components/ComponentsInDepth/ComponentParent.vue";
-import FallthroughAttributes from "./components/ComponentsInDepth/ComponentFallthroughAttributes.vue";
+// import FallthroughAttributes from "./components/ComponentsInDepth/ComponentFallthroughAttributes.vue";
+// import ComponentSlot from "./components/ComponentsInDepth/ComponentSlot.vue";
 </script>
 
 <template>
@@ -23,10 +24,44 @@ import FallthroughAttributes from "./components/ComponentsInDepth/ComponentFallt
     <!-- <Ref /> -->
     <!-- <ComponentParent /> -->
     <!-- <ComponentGlobal /> -->
-    <FallthroughAttributes
+    <!-- <FallthroughAttributes
       class="large"
       id="id"
       @click="console.log('clicked click me')"
-    />
+    /> -->
+
+    <!----------------------- slot ----------------------------------->
+    <!-- <ComponentSlot>
+      <span>Click me</span>
+      <span>+</span>
+      <span>additional</span>
+      <Computed />
+      everything is ok
+    </ComponentSlot> -->
+
+    <!-- <ComponentSlot></ComponentSlot> -->
+    <!-- <button class="fancy-btn">send</button>　-->
+    <!-- <ComponentSlot>保存</ComponentSlot> -->
+    <!-- <button class="fancy-btn">save</button>　-->
+
+    <!--------------------named slot----------------------------->
+    <!-- <ComponentSlot>
+      <p>save</p>
+      <template v-slot:first>
+        <span>first slot</span>
+      </template>
+      <template #second>
+        <span>second slot</span>
+      </template>
+      <template v-slot:[dynamicSlotName]>dynamicSlotName</template>
+    </ComponentSlot> -->
+
+    <!--------------------Scoped Slots-------------------------->
+    <!-- v-slot="slotProps
+    or
+    <ComponentSlot v-slot="{ text, count }">
+      {{ text }} {{ count }}
+      hello 1
+    </ComponentSlot> -->
   </div>
 </template>
---
+++ src/components/ComponentsInDepth/ComponentSlot.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!--------------------named slot --------------------->
+  <!-- <button class="fancy-btn">
+    <slot>submit</slot>
+  </button>
+  <div>
+    <h1>first content</h1>
+    <slot name="first"></slot>
+  </div>
+  <div>
+    <h1>second content</h1>
+    <slot name="second"></slot>
+  </div> -->
+
+  <!--------------------Scoped Slots--------------------->
+  <slot :text="'hello'" :count="1"></slot>
+</template>
+
+<!--
+ComponentSlot({
+  first: `...`,
+  second: `...`,
+})
+
+function ComponentSlot(slots) {
+  return `
+  <div>
+    <div>
+      <h1>first content</h1>
+      <div>${slots.first}</div>
+    </div>
+    <div>
+      <h1>second content</h1>
+      <div>${slots.second}</div>
+    </div>
+  </div>`
+} -->
