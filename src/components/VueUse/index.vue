ac47a6ca4e3c599863629cd234720a4eb927f1d2
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-10-11 20:43:32 +0900

    ✨ useDocumentVisibility

--- src/components/VueUse/index.vue
+++ src/components/VueUse/index.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import UseMouse from "./UseMouse.vue";
 import UseActiveElement from "./UseActiveElement.vue";
+import UseDocumentVisibility from "./UseDocumentVisibility.vue";
 </script>
 
 <template>
   <h1>vue use</h1>
   <!-- <UseMouse /> -->
-  <UseActiveElement />
+  <!-- <UseActiveElement /> -->
+  <UseDocumentVisibility />
 </template>
