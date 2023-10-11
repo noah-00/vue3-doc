38c7a690342430e46c80850fae9f008699c51216
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-10-11 20:35:40 +0900

    ✨ vue use useActiveElement

---
+++ src/components/VueUse/UseActiveElement.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useActiveElement } from "@vueuse/core";
+
+const activeElement = useActiveElement();
+const key = computed(() => activeElement.value?.dataset?.id || "null");
+</script>
+
+<template>
+  <h1>useActiveElement</h1>
+  <div>
+    <input
+      v-for="i in 6"
+      :key="i"
+      type="text"
+      :data-id="i"
+      class="!my-0 !min-w-0"
+      :placeholder="`${i}`"
+    />
+  </div>
+  <div class="mt-2">
+    Current Active Element:
+    <span class="text-primary">{{ key }}</span>
+  </div>
+</template>
--- src/components/VueUse/index.vue
+++ src/components/VueUse/index.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import UseMouse from "./UseMouse.vue";
+import UseActiveElement from "./UseActiveElement.vue";
 </script>
 
 <template>
   <h1>vue use</h1>
-  <UseMouse />
+  <!-- <UseMouse /> -->
+  <UseActiveElement />
 </template>
