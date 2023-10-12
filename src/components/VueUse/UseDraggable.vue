01d12716fe7374b8b3520ca3729dce6171b8fe9a
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-10-12 19:45:09 +0900

    ✨ useDraggable

---
+++ src/components/VueUse/UseDraggable.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useDraggable } from "@vueuse/core";
+
+const el = ref<HTMLElement | null>(null);
+
+// `style` will be a helper computed for `left: ?px; top: ?px;`
+const { x, y, style } = useDraggable(el, {
+  initialValue: { x: 40, y: 40 },
+});
+</script>
+
+<template>
+  <div ref="el" :style="style" style="position: fixed" class="dragging_content">
+    Drag me! I am at {{ x }}, {{ y }}
+  </div>
+</template>
+
+<style scoped>
+.dragging_content {
+  background-color: white;
+  color: black;
+}
+</style>
--- src/components/VueUse/index.vue
+++ src/components/VueUse/index.vue
@@ -2,11 +2,13 @@
 import UseMouse from "./UseMouse.vue";
 import UseActiveElement from "./UseActiveElement.vue";
 import UseDocumentVisibility from "./UseDocumentVisibility.vue";
+import UseDraggable from "./UseDraggable.vue";
 </script>
 
 <template>
   <h1>vue use</h1>
   <!-- <UseMouse /> -->
   <!-- <UseActiveElement /> -->
-  <UseDocumentVisibility />
+  <!-- <UseDocumentVisibility /> -->
+  <UseDraggable />
 </template>
